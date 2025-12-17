1d987f250db7d68f25a215ce3d3e148a708d2b61
sanugi02 <sanugidesilva2002@gmail.com>
2025-12-17 17:17:12 +0530

    Remove node_modules from repository

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
-console.log("Backend server starting...");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("SLIIT GPA Analysis API is running");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
